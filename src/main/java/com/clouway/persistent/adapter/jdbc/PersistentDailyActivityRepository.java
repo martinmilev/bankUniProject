6a484776a5712a5ed0a63f5c847e264746ba35a8
Martin Milev <martinmariusmilev@gmail.com>
2016-11-22 15:45:12 +0200

    bank: Fixed connection leake.
    Found a connection leak which couses the app to freeze.

--- src/main/java/com/clouway/persistent/adapter/jdbc/PersistentDailyActivityRepository.java
+++ src/main/java/com/clouway/persistent/adapter/jdbc/PersistentDailyActivityRepository.java
@@ -2,6 +2,7 @@ package com.clouway.persistent.adapter.jdbc;
 
 import com.clouway.core.DailyActivityRepository;
 import com.clouway.persistent.datastore.DataStore;
+import com.clouway.persistent.datastore.RowFetcher;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -13,25 +14,29 @@ import java.util.Map;
  */
 public class PersistentDailyActivityRepository implements DailyActivityRepository {
   private DataStore dataStore;
+  private Map<String, Integer> transactions = new HashMap();
 
   public PersistentDailyActivityRepository(DataStore dataStore) {
     this.dataStore = dataStore;
   }
 
   public Map<String, Integer> dailyActivity() {
-    String[] types = {"Deposit", "Withdraw"};
-    String query = "select count(operation) from transaction_history as disOpt where operation=? and date >= CURDATE();";
-    Map<String, Integer> transactions = new HashMap();
-    try {
-      for (String each: types) {
-        ResultSet set = dataStore.execute(query, each);
-        if (set.next()) {
-          transactions.put(each, set.getInt(1));
-        }
-      }
-    } catch (SQLException e) {
-      e.printStackTrace();
-    }
+    transactions.put("Deposit", get("Deposit"));
+    transactions.put("Withdraw", get("Withdraw"));
     return transactions;
+  }
+
+  private Integer get(String operation) {
+    String query = "select count(operation) from transaction_history as disOpt where operation='" + operation + "' and date >= CURDATE();";
+    return dataStore.fetchRows(query, new RowFetcher<Integer>() {
+      @Override
+      public Integer fetchRow(ResultSet resultSet) {
+        try {
+          return resultSet.getInt(1);
+        } catch (SQLException e) {
+          e.printStackTrace();
+        } return null;
+      }
+    }).get(0);
   }
 }
